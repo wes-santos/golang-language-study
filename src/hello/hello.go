b13c9ca06959b8e13b41f3faf492a3c386484fc0
Weslley Santos <weslleyalmerindo2@gmail.com>
2025-05-18 11:19:41 -0300

    add initial study files

---
+++ src/hello/hello.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
+
+	return command
+}
+
+func showIntroduction() {
+	name := "Weslley"
+	fmt.Println("Hello,", name+"!")
+}
+
+func showOptions() {
+	fmt.Println("1- Start monitoring")
+	fmt.Println("2- Show logs")
+	fmt.Println("0- Exit program")
+}
+
+func startMonitoring() {
+	fmt.Println("Monitoring")
+	site := "https://httpbin.org/status/404"
+	response, _ := http.Get(site)
+
+	switch response.StatusCode {
+	case http.StatusOK:
+		fmt.Println("The website", site, "is OK.")
+	case http.StatusNotFound:
+		fmt.Println("The website", site, "is returning a 404 status.")
+	default:
+		fmt.Println("Returned status by website not mapped. Value eturned:", response.StatusCode)
+	}
+}
+
+func main() {
+	showIntroduction()
+	showOptions()
+
+	// if command == 1 {
+	// 	fmt.Println("Monitoring...")
+	// } else if command == 2 {
+	// 	fmt.Println("Logging...")
+	// } else if command == 0 {
+	// 	fmt.Println("Exiting program. Bye.")
+	// } else {
+	// 	fmt.Println("Command unrecognized.")
+	// }
+
+	command := readCommand()
+
+	switch command {
+	case 1:
+		startMonitoring()
+	case 2:
+		fmt.Println("Logging...")
+	case 3:
+		fmt.Println("Exiting program. Bye.")
+		os.Exit(0)
+	default:
+		fmt.Println("Command unrecognized.")
+		os.Exit(-1)
+	}
+}
