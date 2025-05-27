1cfddb8b736ef086f4be476aa20caa05621a83b3
Weslley Santos <weslleyalmerindo2@gmail.com>
2025-05-26 22:33:14 -0300

    save logs to a txt file

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -13,11 +14,24 @@ import (
 const timesToMonitoring = 1
 const delayBetweenMonitorings = 5 * time.Second
 
-func readCommand() int {
-	var command int
-	fmt.Scan(&command)
+func main() {
+	showIntroduction()
+	showOptions()
 
-	return command
+	command := readCommand()
+
+	switch command {
+	case 1:
+		startMonitoring()
+	case 2:
+		fmt.Println("Logging...")
+	case 3:
+		fmt.Println("Exiting program. Bye.")
+		os.Exit(0)
+	default:
+		fmt.Println("Command unrecognized.")
+		os.Exit(-1)
+	}
 }
 
 func showIntroduction() {
@@ -31,6 +45,28 @@ func showOptions() {
 	fmt.Println("0- Exit program")
 }
 
+func startMonitoring() {
+	fmt.Println("Monitoring")
+
+	sites, err := readWebsitesFromTextFile()
+	if err != nil {
+		fmt.Printf("Error opening websites.txt file: %v", err)
+	}
+
+	for i := 0; i < timesToMonitoring; i++ {
+		for _, site := range sites {
+			err := checkWebsite(site)
+			if err != nil {
+				fmt.Printf("Error requesting site %s. Error: %v ", site, err)
+			}
+		}
+
+		if i > 0 {
+			time.Sleep(delayBetweenMonitorings)
+		}
+	}
+}
+
 func checkWebsite(site string) error {
 	response, err := http.Get(site)
 	if err != nil {
@@ -39,10 +75,13 @@ func checkWebsite(site string) error {
 	switch response.StatusCode {
 	case http.StatusOK:
 		fmt.Println("The website", site, "is OK.")
+		saveLogsToFile(site, true)
 	case http.StatusNotFound:
 		fmt.Println("The website", site, "is returning a 404 status.")
+		saveLogsToFile(site, false)
 	default:
 		fmt.Println("Returned status by website not mapped. Value returned:", response.StatusCode)
+		saveLogsToFile(site, false)
 	}
 	return nil
 }
@@ -72,44 +111,35 @@ func readWebsitesFromTextFile() ([]string, error) {
 	return websites, nil
 }
 
-func startMonitoring() {
-	fmt.Println("Monitoring")
+func readCommand() int {
+	var command int
+	fmt.Scan(&command)
 
-	sites, err := readWebsitesFromTextFile()
-	if err != nil {
-		fmt.Printf("Error opening websites.txt file: %v", err)
-	}
-
-	for i := 0; i < timesToMonitoring; i++ {
-		for _, site := range sites {
-			err := checkWebsite(site)
-			if err != nil {
-				fmt.Printf("Error requesting site %s. Error: %v ", site, err)
-			}
-		}
-
-		if i > 0 {
-			time.Sleep(delayBetweenMonitorings)
-		}
-	}
+	return command
 }
 
-func main() {
-	showIntroduction()
-	showOptions()
+func saveLogsToFile(site string, status bool) error {
+	file, err := os.OpenFile(
+		"src/logs.txt",
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0666,
+	)
+	defer file.Close()
 
-	command := readCommand()
-
-	switch command {
-	case 1:
-		startMonitoring()
-	case 2:
-		fmt.Println("Logging...")
-	case 3:
-		fmt.Println("Exiting program. Bye.")
-		os.Exit(0)
-	default:
-		fmt.Println("Command unrecognized.")
-		os.Exit(-1)
+	if err != nil {
+		return err
 	}
+
+	timeNow := time.Now().Format("2006-01-02 15:04:05")
+
+	file.WriteString(
+		fmt.Sprintf(
+			"%s - %s - online: %s\n",
+			timeNow,
+			site,
+			strconv.FormatBool(status),
+		),
+	)
+
+	return nil
 }
